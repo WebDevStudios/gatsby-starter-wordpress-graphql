eb5b92f0630be34fe25b43c603db560926dbe7b8
Greg Rickaby <gregrickaby@gmail.com>
2019-08-16 22:25:37 -0500

    use layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,74 @@
-import React from "react";
-import { graphql } from "gatsby";
-import Card from "../components/card";
+import React, { Component } from "react";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/layout";
 
-const Home = ({ data }) => {
-  const posts = data.wordPress.posts.nodes;
+class Home extends Component {
+  render() {
+    const posts = this.props.data.wordpress.posts;
 
-  return (
-    <>
-      {posts.map(post => {
-        return <Card key={post.id} {...post} />;
-      })}
-    </>
-  );
-};
+    return (
+      <Layout>
+        {posts.edges.map(post => (
+          <article>
+            <header>
+              <h2>
+                <Link to={post.node.slug}>{post.node.title}</Link>
+              </h2>
+            </header>
+            <div dangerouslySetInnerHTML={{ __html: post.node.content }} />
+            <footer>
+              <span>
+                <Link to={"/author/" + post.node.author.slug}>
+                  {post.node.author.name}
+                </Link>
+              </span>
+              <time>{post.node.date}</time>
+              <span>
+                {post.node.categories.edges.map(category => (
+                  <Link to={"/category/" + category.node.slug}>
+                    {category.node.name}
+                  </Link>
+                ))}
+              </span>
+              <span>Comments</span>
+            </footer>
+          </article>
+        ))}
+      </Layout>
+    );
+  }
+}
 
 export default Home;
 
 export const pageQuery = graphql`
-  {
+  query {
     wordpress {
-      posts(last: 10) {
-        nodes {
-          id
-          title
-          excerpt
-          slug
+      posts {
+        edges {
+          node {
+            id
+            postId
+            slug
+            status
+            categories {
+              edges {
+                node {
+                  categoryId
+                  name
+                  slug
+                  link
+                }
+              }
+            }
+            title
+            content
+            date
+            author {
+              name
+              slug
+            }
+          }
         }
       }
     }
