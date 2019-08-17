c3567c73c3c78831f03561cfb0b7d97709d56504
Greg Rickaby <gregrickaby@gmail.com>
2019-08-16 22:24:01 -0500

    add 404 template

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const NotFoundPage = () => (
+  <Layout>
+    <h1>I think you broke it!</h1>
+  </Layout>
+);
+
+export default NotFoundPage;
