13c560f45c2a1e322bd671da7c0477761b7ca63b
Greg Rickaby <gregrickaby@gmail.com>
2019-08-16 19:42:25 -0500

    query, and display cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,16 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import Hero from '../components/hero';
-import TeamMembers from '../components/team-members';
+import React from "react";
+import { graphql } from "gatsby";
+import Card from "../components/card";
 
 const Home = ({ data }) => {
-  const sections = data.wordPress.pageBy.pageSectionFields.sections;
-  
+  const posts = data.wordPress.posts.nodes;
+
   return (
-    <div>
-      {sections.map(section => {
-        const typeName = section.__typeName;
-        
-        switch (typeName) {
-          case 'WordPress_Page_Pagesectionfields_Sections_Hero':
-            return <Hero key={section.id} {...section} />
-          
-          case 'WordPress_Page_Pagesectionfields_Sections_TeamMembers':
-            return <TeamMembers key={section.id} {...section} />
-          
-          default:
-            return <p>You done busted it.</p>
-        }
+    <>
+      {posts.map(post => {
+        return <Card key={post.id} {...post} />;
       })}
-    </div>
+    </>
   );
 };
 
@@ -31,43 +19,12 @@ export default Home;
 export const pageQuery = graphql`
   {
     wordPress {
-      pageBy(uri: "home") {
-        id
-        title
-        pageSectionFields {
-          sections {
-            __typename
-
-            ... on WordPress_Page_Pagesectionfields_Sections_Hero {
-              backgroundColor
-              buttonLink
-              buttonText
-              description
-              title
-            }
-
-            ... on WordPress_Page_Pagesectionfields_Sections_TeamMembers {
-              teamMembers {
-                teamMember {
-                  ... on WordPress_TeamMember {
-                    title
-                    id
-                    profileFields {
-                      bio
-                      github
-                      website
-                      twitter
-                      title
-                      photo {
-                        altText
-                        sourceUrl
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
+      posts(last: 10) {
+        nodes {
+          id
+          title
+          excerpt
+          slug
         }
       }
     }
