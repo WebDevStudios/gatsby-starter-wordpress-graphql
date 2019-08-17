2c69f34602cca02ba098d78cf82db08271601f27
Greg Rickaby <gregrickaby@gmail.com>
2019-08-16 22:25:10 -0500

    create layout component

---
+++ src/components/layout.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
+import Header from "./header";
+import Footer from "./footer";
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        wordpress {
+          generalSettings {
+            title
+            description
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Header
+          siteTitle={data.wordpress.generalSettings.title}
+          siteDescription={data.wordpress.generalSettings.description}
+        />
+        <div>
+          <section>
+            <main>{children}</main>
+          </section>
+        </div>
+        <Footer siteTitle={data.wordpress.generalSettings.title} />
+      </>
+    )}
+  />
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Layout;
