df6156a05bf48c37ded1ff39d948448096fdbd7e
Greg Rickaby <gregrickaby@gmail.com>
2019-08-18 14:37:58 -0500

    link home

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "gatsby";
 
 const Header = ({ siteTitle, siteDescription }) => (
   <header>
-    <h1 dangerouslySetInnerHTML={{ __html: siteTitle }} />
+    <h1>
+      <Link to="/" dangerouslySetInnerHTML={{ __html: siteTitle }} />
+    </h1>
     <p dangerouslySetInnerHTML={{ __html: siteDescription }} />
   </header>
 );
