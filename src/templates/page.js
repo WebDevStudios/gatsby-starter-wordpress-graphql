3ce315425d7dcb01f1e22b3ac66926ab404f4300
Greg Rickaby <gregrickaby@gmail.com>
2019-08-18 14:18:52 -0500

    use <Layout> component

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import { graphql } from "gatsby";
+import Layout from "../components/layout";
 
 class PageTemplate extends Component {
   render() {
     const page = this.props.data.wordpress.page;
 
     return (
-      <>
+      <Layout>
         <h1 dangerouslySetInnerHTML={{ __html: page.title }} />
         <div dangerouslySetInnerHTML={{ __html: page.content }} />
-      </>
+      </Layout>
     );
   }
 }
