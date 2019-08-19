1cf37681ed4270578964a530c30720b2e4982998
Greg Rickaby <gregrickaby@gmail.com>
2019-08-19 10:28:35 -0500

    use Byline and Meta components

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
+import { Byline, Meta } from "../components/template-tags";
 
 class PostTemplate extends Component {
   render() {
@@ -11,42 +12,11 @@ class PostTemplate extends Component {
         <article id={post.id}>
           <header>
             <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
-            <div>
-              <time datetime={post.date} pubdate>
-                Published on{" "}
-                <Link to={post.slug} rel="bookmark">
-                  {post.date}
-                </Link>
-              </time>
-              <span>
-                {" "}
-                by{" "}
-                <Link to={"/author/" + post.author.slug}>
-                  <span>{post.author.name}</span>
-                </Link>
-              </span>
-              {null === post.commentCount
-                ? ``
-                : `with ${post.commentCount} comments`}
-            </div>
+            <Byline props={post} />
           </header>
           <section dangerouslySetInnerHTML={{ __html: post.content }} />
           <footer>
-            Filed under{" "}
-            {post.categories.edges.map(category => (
-              <Link
-                key={category.node.slug}
-                to={"/category/" + category.node.slug}
-              >
-                {category.node.name}
-              </Link>
-            ))}{" "}
-            and tagged with{" "}
-            {post.tags.edges.map(tag => (
-              <Link key={tag.node.slug} to={"/tag/" + tag.node.slug}>
-                {tag.node.name}
-              </Link>
-            ))}
+            <Meta props={post} />
           </footer>
         </article>
       </Layout>
