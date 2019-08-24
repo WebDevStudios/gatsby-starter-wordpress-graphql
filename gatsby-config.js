b8d5b16a883d4bf2e9733276161e665e072ea2d3
Greg Rickaby <gregrickaby@gmail.com>
2019-08-24 17:01:22 -0500

    configure gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`
+      }
+    },
+    {
       resolve: "gatsby-source-graphql",
       options: {
         typeName: "WordPress",
