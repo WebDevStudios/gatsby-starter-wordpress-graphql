ecdeb6e3bed4bdbde95e2ac2998463aace53005e
Greg Rickaby <gregrickaby@gmail.com>
2019-09-11 14:26:17 -0500

    remove typography plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,8 @@ module.exports = {
     organization: {
       name: "WebDevStudios",
       url: "https://gatsby.wdslab.com",
-      logo: "http://gatsby.wdslab.com/wp-content/uploads/2019/08/wds-logo-small.png"
+      logo:
+        "http://gatsby.wdslab.com/wp-content/uploads/2019/08/wds-logo-small.png"
     },
     social: {
       twitter: "@webdevstudios",
@@ -29,16 +30,10 @@ module.exports = {
         refetchInterval: 60
       }
     },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      }
-    },
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-emotion",
     "gatsby-plugin-sitemap",
-    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-theme-ui"
   ]
 };
--- src/config/styles.js
+++
@@ -1,7 +0,0 @@
-export const colors = {
- test: '#333333',
-};
-
-export default {
-  colors,
-}
