4b04c25ce558f8d9d4c31c32eb316d15023c9119
Greg Rickaby <gregrickaby@gmail.com>
2019-09-11 14:27:10 -0500

    include color toggle button

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
 import Menu from "./menu";
+import { useColorMode } from "theme-ui";
 
-const Header = ({ siteTitle, siteDescription }) => (
-  <header>
-    <h1>
-      <Link to="/" dangerouslySetInnerHTML={{ __html: siteTitle }} />
-    </h1>
-    <p dangerouslySetInnerHTML={{ __html: siteDescription }} />
-    <Menu />
-  </header>
-);
+const Header = ({ siteTitle, siteDescription }) => {
+  const [colorMode, setColorMode] = useColorMode();
+
+  return (
+    <header>
+      <h1>
+        <Link to="/" dangerouslySetInnerHTML={{ __html: siteTitle }} />
+      </h1>
+      <p dangerouslySetInnerHTML={{ __html: siteDescription }} />
+      <Menu />
+      <button
+        onClick={e => {
+          setColorMode(colorMode === "light" ? "dark" : "light");
+        }}
+      >
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </button>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
