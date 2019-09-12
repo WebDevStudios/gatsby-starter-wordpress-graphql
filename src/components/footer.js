791f92737a11d7936b00107717cb2729c4d4d27a
Greg Rickaby <gregrickaby@gmail.com>
2019-09-12 16:34:08 -0500

    style the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+/** @jsx jsx */
+import { jsx, Container } from "theme-ui";
 
 const Footer = ({ siteTitle }) => (
-  <footer>
-    Copyright {new Date().getFullYear()} &middot; {siteTitle} &middot; Powered
-    by Gatsby, GraphQL, and WordPress.
+  <footer sx={{ variant: "layout.footer" }}>
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        py: 1
+      }}
+    >
+      <p>
+        Copyright {new Date().getFullYear()} &middot; {siteTitle} &middot;
+        Powered by Gatsby, GraphQL, and WordPress.
+      </p>
+    </Container>
   </footer>
 );
 
