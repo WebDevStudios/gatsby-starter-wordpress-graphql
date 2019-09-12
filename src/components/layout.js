337f8d7e58efb000714ba07a331f3086d0e390b2
Greg Rickaby <gregrickaby@gmail.com>
2019-09-12 16:47:09 -0500

    Add <Global> to remove default browser styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
 import Header from "./header";
 import Footer from "./footer";
+import { Global } from "@emotion/core";
 import { Layout as ThemeLayout, Container, Main } from "theme-ui";
 
 const Layout = ({ children }) => (
@@ -18,16 +19,25 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <ThemeLayout>
-        <Header
-          siteTitle={data.wordpress.generalSettings.title}
-          siteDescription={data.wordpress.generalSettings.description}
+      <>
+        <Global
+          styles={{
+            body: {
+              margin: 0
+            }
+          }}
         />
-        <Container>
-          <Main>{children}</Main>
-        </Container>
-        <Footer siteTitle={data.wordpress.generalSettings.title} />
-      </ThemeLayout>
+        <ThemeLayout>
+          <Header
+            siteTitle={data.wordpress.generalSettings.title}
+            siteDescription={data.wordpress.generalSettings.description}
+          />
+          <Container>
+            <Main>{children}</Main>
+          </Container>
+          <Footer siteTitle={data.wordpress.generalSettings.title} />
+        </ThemeLayout>
+      </>
     )}
   />
 );
