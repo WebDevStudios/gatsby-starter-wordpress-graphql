a478ac1f3d64f9c904fee1808ec8bd980a77f1e7
Greg Rickaby <gregrickaby@gmail.com>
2019-09-13 09:23:56 -0500

    style meta

--- src/components/meta.js
+++ src/components/meta.js
@@ -1,27 +1,43 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
+/** @jsx jsx */
+import { jsx, Flex } from "theme-ui";
 
 const Meta = ({ props }) => (
-  <>
-    Filed under{" "}
-    <ul>
+  <Flex>
+    <span>Filed under </span>
+    <ul sx={{ listStyle: "none", padding: 0, margin: 0 }}>
       {props.categories.edges.map(category => (
-        <li key={category.node.slug}>
+        <li
+          sx={{
+            display: "inline",
+            ml: 1,
+            mr: 1
+          }}
+          key={category.node.slug}
+        >
           <Link to={"/category/" + category.node.slug}>
             {category.node.name}
           </Link>
         </li>
-      ))}{" "}
+      ))}
     </ul>
-    and tagged with{" "}
-    <ul>
+    <span> and tagged with </span>
+    <ul sx={{ listStyle: "none", padding: 0, margin: 0 }}>
       {props.tags.edges.map(tag => (
-        <li key={tag.node.slug}>
+        <li
+          sx={{
+            display: "inline",
+            ml: 1,
+            mr: 1
+          }}
+          key={tag.node.slug}
+        >
           <Link to={"/tag/" + tag.node.slug}>{tag.node.name}</Link>
         </li>
       ))}
     </ul>
-  </>
+  </Flex>
 );
 
 export default Meta;
